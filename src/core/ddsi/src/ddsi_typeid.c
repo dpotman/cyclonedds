b78877395852a05a99d284301b242e560e0dd359
Dennis Potman <dennis.potman@adlinktech.com>
2020-08-03 11:52:49 +0200

    malloc -> ddsrt_malloc
    Signed-off-by: Dennis Potman <dennis.potman@adlinktech.com>

--- src/core/ddsi/src/ddsi_typeid.c
+++ src/core/ddsi/src/ddsi_typeid.c
@@ -18,7 +18,7 @@
 type_identifier_t * ddsi_typeid_from_sertype (const struct ddsi_sertype *type)
 {
   assert (type != NULL);
-  type_identifier_t *type_id = malloc (sizeof (*type_id));
+  type_identifier_t *type_id = ddsrt_malloc (sizeof (*type_id));
   type->ops->typeid_hash (type, type_id->hash);
   return type_id;
 }
