15c2fde6dd1a9544e378892c79e594b18c4be63f
Dennis Potman <dennis.potman@adlinktech.com>
2021-05-27 14:19:24 +0200

    [wip] idlc test fixes for windows
    Signed-off-by: Dennis Potman <dennis.potman@adlinktech.com>

--- src/tools/idlc/tests/Compile.cmake
+++ src/tools/idlc/tests/Compile.cmake
@@ -19,16 +19,14 @@ if(NOT _idl_compiler)
   message(FATAL_ERROR "IDL compiler not set")
 endif()
 
-# Use C_INCLUDE_PATH environment variable to forward directories with GCC
-# Use INCLUDE environment variable to forward directories with MSVC
-#string(REGEX REPLACE ":+" ";" _paths "$ENV{C_INCLUDE_PATH};$ENV{INCLUDE}")
-
 if(WIN32)
   set(_separator ";")
 else()
   set(_separator ":")
 endif()
 
+# Use C_INCLUDE_PATH environment variable to forward directories with GCC
+# Use INCLUDE environment variable to forward directories with MSVC
 foreach(_path $ENV{C_INCLUDE_PATH} $ENV{INCLUDE} ${CMAKE_CURRENT_BINARY_DIR})
   if(_c_include_path)
     set(_c_include_path "${_c_include_path}${_separator}${_path}")
@@ -40,12 +38,17 @@ endforeach()
 # set compiler specific values
 if(_c_compiler MATCHES "cl\\.exe$")
   set(ENV{INCLUDE} "${_c_include_path}")
+  set(ENV{PATH} "$ENV{PATH};$ENV{CDDS_BIN}")
   set(_output_flag "/Fe")
+  set(_lib_path "/link /LIBPATH:$ENV{LIBRARY_PATH}")
+  set(_lib "ddsc.lib")
 else()
   set(ENV{C_INCLUDE_PATH} "${_c_include_path}")
+  set(ENV{LD_LIBRARY_PATH} "$ENV{LD_LIBRARY_PATH}:$ENV{LIBRARY_PATH}")
   set(_output_flag "-o")
+  set(_lib_path "")
+  set(_lib "-lddsc")
 endif()
-
 
 # get list of sources from argument list
 set(_skip_next FALSE)
@@ -66,12 +69,6 @@ if(NOT _sources)
   message(FATAL_ERROR "no source files specified")
 endif()
 
-# C:\Program Files (x86)\Windows Kits\NETFXSDK\4.8\lib\um\x86;
-# LIB=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.28.29333\ATLMFC\lib\x86;
-
-#set(ENV{LIB} "C:\Program Files (x86)\Windows Kits\10\lib\10.0.18362.0\ucrt\x64;C:\Program Files (x86)\Windows Kits\10\lib\10.0.18362.0\um\x64")
-#set(ENV{LIB} "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.28.29333\lib\x64;C:\Program Files (x86)\Windows Kits\10\lib\10.0.18362.0\ucrt\x64;C:\Program Files (x86)\Windows Kits\10\lib\10.0.18362.0\um\x64")
-
 foreach(_source ${_sources})
   get_filename_component(_base "${_source}" NAME_WE)
   set(_main "${CMAKE_CURRENT_BINARY_DIR}/${_base}.main.c")
@@ -87,9 +84,11 @@ foreach(_source ${_sources})
     message(FATAL_ERROR "Cannot transpile ${_source} to source code")
   endif()
 
+  message("path: $ENV{PATH}")
+
   # compile and link c files
   execute_process(
-    COMMAND "${_c_compiler}" ${_output_flag}${_base} "${_main}" "${_c}" "-lddsc"
+    COMMAND "${_c_compiler}" ${_output_flag}${_base} "${_main}" "${_c}" "${_lib}" "${_lib_path}"
     COMMAND_ECHO STDOUT
     RESULT_VARIABLE _result)
   if(NOT _result EQUAL "0")
