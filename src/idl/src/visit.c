70ee34043eb16a626c7ab03913693adff22badd7
Dennis Potman <dennis.potman@adlinktech.com>
2021-06-08 17:36:33 +0200

    Add bit_value to idl visitor
    Signed-off-by: Dennis Potman <dennis.potman@adlinktech.com>

--- src/idl/src/visit.c
+++ src/idl/src/visit.c
@@ -58,6 +58,8 @@ static idl_accept_t idl_accept(const void *node)
     return IDL_ACCEPT_ENUM;
   if (mask & IDL_BITMASK)
     return IDL_ACCEPT_BITMASK;
+  if (mask & IDL_BIT_VALUE)
+    return IDL_ACCEPT_BIT_VALUE;
   return IDL_ACCEPT;
 }
 
