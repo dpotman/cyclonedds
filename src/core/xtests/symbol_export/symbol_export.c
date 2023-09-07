569615ee45f4d58fd8d7e8de09f8c69b11363c91
Phil <25635927+poetinger@users.noreply.github.com>
2023-09-07 11:06:28 +0200

    Introduce ddsrt_allocation_ops and ddsrt_set_allocators (#1821)
    * Introduce ddsrt_allocation_ops and ddsrt_set_allocators
    
    This commit introduces the `ddsrt_allocation_ops_t` struct, which
    encapsulates memory allocation function pointers (malloc, calloc,
    realloc, free). Additionally the user may change these from the default
    functions of `stdlib` with the new function `ddsrt_set_allocators`.
    
    The new struct makes it easier to change allocation
    strategies.
    
    Changes:
    - Added `ddsrt_allocation_ops_t` struct
    - Added `ddsrt_set_allocators(ddsrt_allocation_ops)`
    - Added `static ddsrt_allocator`, with default members from `stdlib`
    - Changed `stdlib` allocator calls to use `ddsrt_allocator.{function}` etc.
    
    CP-221
    
    * Add `ddsrt_set_allocator` to symbol_export
    
    CP-221

--- src/core/xtests/symbol_export/symbol_export.c
+++ src/core/xtests/symbol_export/symbol_export.c
@@ -988,6 +988,7 @@ int main (int argc, char **argv)
 #endif
 
   // ddsrt/heap.h
+  ddsrt_allocation_ops_t ao = {0,0,0,0};
   ddsrt_malloc (1);
   ddsrt_malloc_s (1);
   ddsrt_calloc (1, 1);
@@ -995,6 +996,7 @@ int main (int argc, char **argv)
   ddsrt_realloc (ptr, 1);
   ddsrt_realloc_s (ptr, 1);
   ddsrt_free (ptr);
+  ddsrt_set_allocator(ao);
 
   // ddsrt/string.h
   ddsrt_strcasecmp (ptr, ptr);
